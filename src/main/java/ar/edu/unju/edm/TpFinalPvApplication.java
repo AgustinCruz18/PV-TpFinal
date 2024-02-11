792286afb0fc4349f7ded280b4efc362fd39182a
agust <agust@AGUSTIN>
2024-02-11 03:39:46 -0300

    Agregacion de controllers

---
+++ src/main/java/ar/edu/unju/edm/TpFinalPvApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpFinalPvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpFinalPvApplication.class, args);
+	}
+
+}
